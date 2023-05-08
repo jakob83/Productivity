b9de5a4c7de86985f568cacd2e76e458dfa08c76
jakob83 <jakobpllogmail.com>
2023-05-08 21:09:25 +0200

    index.html

--- index.js
+++ index.js
@@ -23,14 +23,95 @@ addRowButton.addEventListener("click", function addRow(){
 document.getElementById("calc1").addEventListener("click", function(){
 
     inputs = document.getElementsByClassName("input")
-    counter1 = 0
+    counter = 0
 
     for(i=0;i<inputs.length;i+=7){
         value = Number(inputs[i].value) 
-        counter1 += value
+        counter += value
 }
-    console.log(counter1+" Seiten")
+    console.log(counter+" Seiten")
 }
 )
 
 
+document.getElementById("calc2").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+    counter = 0
+
+    for(i=1;i<inputs.length;i+=7){
+        value = Number(inputs[i].value) 
+        counter += value
+}
+    console.log(counter+" Minuten")
+}
+)
+
+document.getElementById("calc3").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+
+    counter = 0
+    for(i=2;i<inputs.length;i+=7){
+        value = inputs[i].value
+        if(value==="Ja"){
+            counter+=1
+        }
+    }
+        console.log(counter+" Mal verfehlt")
+}
+)
+
+document.getElementById("calc4").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+
+    counter = 0
+    for(i=3;i<inputs.length;i+=7){
+        value = inputs[i].value
+        if(value==="Ja"){
+            counter+=1
+        }
+    }
+        console.log(counter+" Mal X")
+}
+)
+
+document.getElementById("calc5").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+    counter = 0
+
+    for(i=4;i<inputs.length;i+=7){
+        value = Number(inputs[i].value) 
+        counter += value
+}
+    console.log(counter+" Minuten")
+}
+)
+
+document.getElementById("calc6").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+    counter = 0
+
+    for(i=5;i<inputs.length;i+=7){
+        value = Number(inputs[i].value) 
+        counter += value
+}
+    console.log(counter+" Minuten")
+}
+)
+
+document.getElementById("calc7").addEventListener("click", function(){
+
+    inputs = document.getElementsByClassName("input")
+    counter = 0
+
+    for(i=6;i<inputs.length;i+=7){
+        value = Number(inputs[i].value) 
+        counter += value
+}
+    console.log(counter+" Minuten")
+}
+)
